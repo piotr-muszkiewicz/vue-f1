38acfca67fc06ef21bea16e3968755640a37f776
Piotr Muszkiewicz <info@piotr-m.pl>
2019-01-10 18:23:28 +0100

    Component in app

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,24 @@
 <template>
   <div id="app">
-    <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <router-link class="navbar-logo" to="/">
-          <img src="./assets/img/f1_logo.png" alt>
-        </router-link>
-      </div>
-
-      <div id="navbar-menu" class="navbar-menu">
-        <div class="navbar-start is-centered">
-          <router-link class="navbar-item" to="/seasons">Seasons</router-link>
-          <router-link class="navbar-item" to="/drivers">Drivers</router-link>
-          <router-link class="navbar-item" to="/teams">Teams</router-link>
-        </div>
-      </div>
-    </nav>
+    <Header></Header>
     <main class="container">
       <router-view/>
     </main>
-    <footer class="footer">
-      <div class="content has-text-centered">
-        <p>
-          <strong>F1 Data</strong> by
-          <a href="https://piotr-m.pl">rFront Piotr Muszkiewicz</a>.
-        </p>
-      </div>
-    </footer>
+    <Footer></Footer>
   </div>
 </template>
+
+<script>
+  import Header from "@/components/Header";
+  import Footer from "@/components/Footer";
+    export default {
+    name: "App",
+    components: {
+      Header,
+      Footer
+    }
+  };
+</script>
 
 <style lang="scss">
 #app {
@@ -41,24 +32,5 @@
     position: relative;
   }
 }
-.navbar {
-  flex-wrap: wrap;
-}
-.navbar-brand {
-  width: 100%;
-  text-align: center;
-  .navbar-logo {
-    margin: 0 auto;
-    height: 80px;
-    img {
-      max-width: 200px;
-      margin-top: -30px;
-    }
-  }
-}
-.navbar-menu {
-  .navbar-start {
-    margin: 0 auto;
-  }
-}
+
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>F1 Data</strong> by
+          <a href="https://piotr-m.pl">rFront Piotr Muszkiewicz</a>.
+        </p>
+      </div>
+    </footer>
+</template>
