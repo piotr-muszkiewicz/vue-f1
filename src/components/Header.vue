10ccfa4c550b8a9be276bd6d173b2b26786ce73e
Piotr Muszkiewicz <info@piotr-m.pl>
2019-01-10 18:28:15 +0100

    fix eslint

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,40 +1,40 @@
 <template>
-    <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <router-link class="navbar-logo" to="/">
-          <img src="../assets/img/f1_logo.png" alt>
-        </router-link>
-      </div>
+  <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <router-link class="navbar-logo" to="/">
+        <img src="../assets/img/f1_logo.png" alt>
+      </router-link>
+    </div>
 
-      <div id="navbar-menu" class="navbar-menu">
-        <div class="navbar-start is-centered">
-          <router-link class="navbar-item" to="/seasons">{{$t('title.seasons')}}</router-link>
-          <router-link class="navbar-item" to="/drivers">{{$t('title.drivers')}}</router-link>
-          <router-link class="navbar-item" to="/teams">{{$t('title.teams')}}</router-link>
-        </div>
+    <div id="navbar-menu" class="navbar-menu">
+      <div class="navbar-start is-centered">
+        <router-link class="navbar-item" to="/seasons">{{$t('title.seasons')}}</router-link>
+        <router-link class="navbar-item" to="/drivers">{{$t('title.drivers')}}</router-link>
+        <router-link class="navbar-item" to="/teams">{{$t('title.teams')}}</router-link>
       </div>
-      <LangSelect></LangSelect>
-    </nav>
+    </div>
+    <LangSelect></LangSelect>
+  </nav>
 </template>
 
 <script>
-  import LangSelect from "@/components/LangSelect";
-  export default {
-    name: "navigation",
-    components: {
-      LangSelect
-    }
-  };
+import LangSelect from "@/components/LangSelect";
+export default {
+  name: "navigation",
+  components: {
+    LangSelect
+  }
+};
 </script>
 
 <style lang="scss">
 .navbar {
   flex-wrap: wrap;
   .locale-changer {
-      position: absolute;
-      right: 15px;
-      top: 50%;
-      transform: translateY(-50%);
+    position: absolute;
+    right: 15px;
+    top: 50%;
+    transform: translateY(-50%);
   }
 }
 .navbar-brand {
